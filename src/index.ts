c655be683e33bf1a6d062a3db95f37e5d278cc4f
kamal-hamza <kamal.hamza@outlook.com>
2026-03-18 18:04:22 -0700

    feat: make Pseudocode the default export in src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-export { Pseudocode } from "./transformers/Pseudocode";
+import { Pseudocode } from "./transformers/Pseudocode";
+export { Pseudocode };
+export default Pseudocode;
 export type { PseudoOptions } from "./transformers/Pseudocode";
 export type { QuartzTransformerPlugin } from "@quartz-community/types";
